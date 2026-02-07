8ed3305b79f1be1a6914a9ee76238513bd9d55a5
SAURABHXGFX <bloodsk09@gmail.com>
2026-02-07 15:02:55 +0530

    Refactor vite.config.ts for improved structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
   base: "/VALENTINE-PROJECT/",
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
